5b083459c6e44b2cbcbd8699972293c136143bd4
Jenna Smith <jenna@growldesign.co.uk>
2012-11-13 13:36:06 +0000

    adding before and after methods

--- jquery.animateEach.js
+++ jquery.animateEach.js
@@ -1,37 +1,46 @@
-/*
-* jQuery animateEach Plugin
-* Example at: http://jsfiddle.net/JJenZz/5dEfs/
-* Copyright (c) 2010 J. Smith (@jjenzz with help from @rwaldin)
-* Dual licensed under the MIT and GPL licenses:
-* http://www.opensource.org/licenses/mit-license.php
-* http://www.gnu.org/licenses/gpl.html
-*
-* --------------------------------------------------------------------------
-* .animateEach(properties, [ duration ], [ callback ])
-* --------------------------------------------------------------------------
-* properties: A map of CSS properties that the animation will move toward.
-* duration: A string or number determining how long the animation will run.
-* callback: A function to call once the animation is complete.
-*
-* Example:-
-* --------------------------------------------------------
-* $('#demo li').animateEach({'font-size': '22px'}, function() {
-*    $(this).css('background-color', '#f00');
-* });
-*
-*/
-
+/*!------------------------------------------------------
+ * jQuery animateEach v1.0.0
+ * http://github.com/jjenzz/jQuery.animateEach
+ * ------------------------------------------------------
+ * Copyright (c) 2012 J. Smith (@jjenzz)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * --------------------------------------------------------------------------
+ * .animateEach(properties [, options ] [, callback ])
+ * --------------------------------------------------------------------------
+ * properties: A map of CSS properties that each animation will move toward.
+ * options: An object containing options to merge with the defaults.
+ * callback: A function to call once all animations are complete.
+ */
 (function($) {
-	$.fn.animateEach = function(properties, duration, callback) {
-		var i = 0, el = this;
-		
-		callback = $.proxy(callback || (typeof duration === 'function' ? duration : $.noop), this);
-		duration = typeof duration !== 'function' ? duration : 300;
+	$.fn.animateEach = function(properties, options, callback) {
+		var i = 0, el = this, len = el.length, item, opts;
 
-		(function() {
-			el.eq(i++).animate(properties, duration, el.length > i ? arguments.callee : callback);
+		opts = $.extend({}, $.fn.animateEach.defaults, options);
+		callback = callback || (typeof options === 'function' ? options : $.noop);
+
+		(function go() {
+			item = el.eq(i);
+			opts.before.call(item);
+			item.animate(properties, opts.duration, function() {
+				if (i < len) {
+					opts.after.call(item);
+					go();
+				} else {
+					callback.call(el);
+				}
+			});
+			i++;
 		})();
 
 		return this;
+	};
+
+	$.fn.animateEach.defaults = {
+		duration: 300, 	// A string or number determining how long each animation will run
+		before: $.noop,	// A function that will be called before each animation is run.
+		after: $.noop		// A function that will be called after each animation is run.
 	};
 }(jQuery));
